6fabc05925d45da4705aa22e6a048b0061d90ad7
Carolina Lopes <ccal2@cin.ufpe.br>
2019-09-28 16:00:43 -0300

    Create Unit Test template

---
+++ Unit Test.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,29 @@
+//___FILEHEADER___
+
+import XCTest
+@testable import ___PROJECTNAME___
+
+class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+
+    // MARK: - Sut
+
+    // MARK: - Variables
+
+    // MARK: - Set up
+
+    override func setUp() {
+        
+    }
+
+    // MARK: - Tear down
+
+    override func tearDown() {
+        
+    }
+
+    // MARK: - Tests
+
+    func testExample() {
+        
+    }
+}
